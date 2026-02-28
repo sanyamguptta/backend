401b35a3235c8373eb0c327367412a0d2bf991a6
Sanyam <sanyamg2018@gmail.com>
2026-02-28 13:13:06 +0530

    Update app.js

--- day-4/src/app.js
+++ day-4/src/app.js
@@ -3,6 +3,9 @@
 const express = require('express')
 const app = express();
 
+// for reading data of req.body
+// using app.use as middleware
 app.use(express.json());
 
+// exporting and using this fiile code in server.js file
 module.exports = app;
