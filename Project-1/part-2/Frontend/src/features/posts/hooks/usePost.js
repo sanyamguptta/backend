72d1c21c2fd292410c3b429d51ba0905fad89f80
Sanyam <sanyamg2018@gmail.com>
2026-03-18 05:59:39 +0530

    update

--- Project-1/part-2/Frontend/src/features/posts/hooks/usePost.js
+++ Project-1/part-2/Frontend/src/features/posts/hooks/usePost.js
@@ -1,5 +1,5 @@
 // hook layer -> for managuing states and functions related to posts
-import { createPost, getFeed } from "../services/post.api"; // importing getFeed() function to call it inside custom hook
+import { createPost, getFeed, likePost } from "../services/post.api"; // importing getFeed() function to call it inside custom hook
 import { useContext } from "react"; // importing useContext() hook to use context API in custom hook
 import { PostContext } from "../post.context"; // importing PostContext to use it inside custom hook
 
@@ -29,13 +29,13 @@ export const usePost = () => {
   };
 
   //creating function to call likePost() function and set post data in state variable
-  const handleLike = asymc (postId) => {
+  const handleLike = async (postId) => {
 
     setLoading(true);
     // calling likePost() function to send post id to backend and get post data in response, and then we will set post data in post state variable
     const data = await likePost(postId);
-    setLosading(false);
-  }
+    setLoading(false);
+  };
 
   //creating function to call unlikePost() function and set post data in state variable
   const handleUnlike = async (postId) => {
